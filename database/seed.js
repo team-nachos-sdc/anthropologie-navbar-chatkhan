c59338e47eca92ed295768e327a964c410acd782
Emily Yang <emilyyang@Emilys-MacBook-Air.local>
2019-08-21 12:18:30 -0700

    added file structure

---
+++ database/seed.js
@@ -0,0 +1,26 @@
+//product category ids
+//1 - 25es
+//26-50 Pants
+//51-75 Skirts
+//76-100 Bedding
+​
+//product categories
+const productCategories = [
+  'Dress', 'Pants', 'Skirts', 'Bedding'
+]
+//product subcategories
+const dressesSubcategory = [
+  'Maxi', 'Midi', 'Mini',
+  'Sheath', 'Pleated'
+]
+const pantsSubcategory = [
+  'Pants', 'Trousers', 'Joggers', 'Jeans', 
+  'Jumpsuit', 'Overalls', 'Leggings'
+]
+const skirtsSubcategory = [
+  'Midi Skirt', 'Maxi Skirt', 'Pencil Skirt', 'Mini Skirt', 
+  'Wrap Skirt'
+]
+const BeddingSubcategory = [
+  'Quilt', 'Cover', 'Blanket'
+]
