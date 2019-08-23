be56878368c4fafba5ed9039e33b9cc34c238d29
Emily Yang <emilyyang@Emilys-MacBook-Air.local>
2019-08-23 10:51:49 -0700

    added images and mouseover

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -5,18 +5,35 @@ export default class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: ''
+      query: '',
+      moused: false
     }
+    this.mouseOut = this.mouseOut.bind(this);
+    this.mouseOver = this.mouseOver.bind(this);
+
   }
 
+  mouseOut() {
+    console.log("Mouse out!!!");
+    this.setState({ moused: false });
+  }
+
+  mouseOver() {
+    console.log("Mouse over!!!");
+    this.setState({ moused: true });
+  }
 
   render() {
     return (
       <div className="search-container">
-        <form action="/action_page.php">
-          <input type="text" placeholder="search" className="search"></input>
-        </form>
-
+        <span onMouseOut={this.mouseOut} onMouseOver={this.mouseOver}>
+          {this.state.moused ? <img src={'./HoverSearch.png'}></img> : <img src={'./Search.png'}></img>}
+        </span>
+        <span>
+          <form action="/action_page.php">
+            <input type="text" placeholder="search" className="search"></input>
+          </form>
+        </span>
       </div>
     )
   }
--- client/src/components/SiteHeader.js
+++ client/src/components/SiteHeader.js
@@ -1,25 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Search from './Search.js';
 
+export default class SiteHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      bagMoused: false,
+      dropMoused: false
 
-const SiteHeader = (props) => {
-  return (
-    <div className="header-container">
-      <Search />
-      <span id="brand">ANTHROPOLOGIE</span>
-      <img src="../../dist/anthropologies-web-logo.png"></img>
-      <div id="right-header">
-        <a href="#" className="header-link">registry</a>
-        <a href="#" className="header-link">usd: english</a>
-        {/* <div class="dropdown-content">
+    }
+    this.mouseOutDropdown = this.mouseOutDropdown.bind(this);
+    this.mouseOverDropdown = this.mouseOverDropdown.bind(this);
+    this.mouseOutBag = this.mouseOutBag.bind(this);
+    this.mouseOverBag = this.mouseOverBag.bind(this);
+
+  }
+
+  mouseOutDropdown() {
+    console.log("Mouse out!!!");
+    this.setState({ dropMoused: false });
+  }
+
+  mouseOverDropdown() {
+    console.log("Mouse over!!!");
+    this.setState({ dropMoused: true });
+  }
+  mouseOutBag() {
+    console.log("Mouse out!!!");
+    this.setState({ bagMoused: false });
+  }
+
+  mouseOverBag() {
+    console.log("Mouse over!!!");
+    this.setState({ bagMoused: true });
+  }
+
+  render() {
+    return (
+      <div className="header-container">
+        <Search />
+        <img src={"./brand.png"} />
+        <div id="right-header">
+          <a href="#" className="header-link">registry</a>
+          <a href="#" className="header-link">usd: english</a>
+          {/* <div class="dropdown-content">
     <a href="#">Link 1</a>
     <a href="#">Link 2</a>
     <a href="#">Link 3</a>
   </div> */}
-        <a href="#" className="header-link">sign in / sign up </a>
+          <span id="dropdownTriangle" onMouseOut={this.mouseOutDropdown} onMouseOver={this.mouseOverDropdown}>
+            {this.state.dropMoused ? <img src={'./Triangle.png'}></img> : <img src={'./DownTriangle.png'}></img>}
+          </span>
+          <a href="#" className="header-link">sign in / sign up </a>
+          <span id="shoppingBag" onMouseOut={this.mouseOutBag} onMouseOver={this.mouseOverBag}>
+            {this.state.bagMoused ? <img src={'./HoverBag.png'}></img> : <img src={'./Bag.png'}></img>}
+          </span>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
-export default SiteHeader;
